77c9ab2e1d9681a1834166876b6eca5d179e2c75
Anas Iqbal <theanasiqball@gmail.com>
2025-07-08 16:59:30 +0530

    fixed token error

--- src/app/api/facebook/callback/route.ts
+++ src/app/api/facebook/callback/route.ts
@@ -3,17 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 import { prisma } from "@/lib/prisma";
 
-export async function GET(req: NextRequest) {
-    const token = await prisma.token.findFirst({
-        where:{
-            appName:"facebook"
-        }
-    })
-
-    const fb_token = token?.accessToken
-
-    
-
+export async function GET(req: NextRequest) {    
   const code = req.nextUrl.searchParams.get("code");
   const CLIENT_ID = process.env.FACEBOOK_CLIENT_ID!;
   const CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET!;
